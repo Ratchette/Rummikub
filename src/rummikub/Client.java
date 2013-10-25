a7c50deeef361a85eee6e725692aac6ad9e1c305
Ratchette <jenn.winer@gmail.com>
2013-10-24 23:22:49 -0400

    missed some files ...

---
+++ src/rummikub/Client.java
@@ -0,0 +1,24 @@
+/**
+ * Description: CIS*4900 - A Rummikub framework
+ * @author Jennfier Winer
+ * 
+ * @since 2013-10-24
+ * Created: October 24, 2013
+ * Last Modified: October 24, 2013
+ */
+
+package rummikub;
+
+/**
+ * A class that models a Rummikub client.
+ * 
+ * @author jwiner
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/rummikub/Server.java
@@ -0,0 +1,25 @@
+/**
+ * Description: CIS*4900 - A Rummikub framework
+ * @author Jennfier Winer
+ * 
+ * @since 2013-10-24
+ * Created: October 24, 2013
+ * Last Modified: October 24, 2013
+ */
+
+package rummikub;
+
+/**
+ * A class that models a Rummikub server.
+ * The server performs the game setup as well as the validation of moves
+ * 
+ * @author jwiner
+ */
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
